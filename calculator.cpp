c8118613fd227d5c3b76e69a2b3b132ed657a633
Ar1zant <arta849@mail.ru>
2026-05-29 01:00:29 +0300

    Update from branch B: alternative name for Matrix A

--- calculator.cpp
+++ calculator.cpp
@@ -47,11 +47,7 @@ int main() {
     vector<vector<int>> matrixB(rows, vector<int>(cols));
 
     // Ввод данных
-<<<<<<< HEAD
-    cout << "\n--- Первая Матрица ---" << endl;
-=======
     cout << "\n--- Матрица №1 ---" << endl;
->>>>>>> branch-B
     inputMatrix(matrixA, rows, cols);
 
     cout << "\n--- Матрица B ---" << endl;
